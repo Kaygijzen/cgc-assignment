eae24b75eeb905d2ce695546b637ec6735ea540b
HPC course Leiden <hpcl2317@fs0.cm.cluster>
2023-12-10 12:03:58 +0100

    remove unused imports

--- src/cuda/module.h
+++ src/cuda/module.h
@@ -1,15 +1,4 @@
-#ifndef _MODULE_H_
-#define _MODULE_H_
-
-#ifdef __CUDACC__
-#define CUDA_GLOBAL __global__
-#else
-#define CUDA_GLOBAL
-#endif
-
 #include <iostream>
-#include <numeric>
-#include <cuda.h>
 
 std::pair<int, double> best_row_label(
 	int num_row_labels,
@@ -20,5 +9,3 @@ std::pair<int, double> best_row_label(
 	const float* cluster_avg,
 	int i,
 	const int* col_labels); 
-
-#endif
