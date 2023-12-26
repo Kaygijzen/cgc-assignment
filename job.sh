ce4af1e52943d2c850f153e03760557b5d9a5b0b
HPC course Leiden <hpcl2317@fs0.cm.cluster>
2023-12-26 11:31:29 +0100

    test

--- job.sh
+++ job.sh
@@ -3,4 +3,4 @@
 #SBATCH -N 16
 #SBATCH --ntasks-per-node=1
 
-mpirun -np 16 bin/cgc_mpi /var/scratch/bwn200/HPC_data/spring_data_m.npy /var/scratch/bwn200/HPC_data/spring_labels_m_5x100.txt --max-iterations 125 --output "mpi.txt"
+mpirun -np 16 bin/cgc_mpi /var/scratch/bwn200/HPC_data/spring_data_l.npy /var/scratch/bwn200/HPC_data/spring_labels_l_5x100.txt --max-iterations 125 --output "mpi.txt"
