663f89d029f77af85f62df6ac73517280280dcfa
HPC course Leiden <hpcl2317@node004.cm.cluster>
2024-01-15 11:46:16 +0100

    wip row_labels|

--- src/cuda/module.cu
+++ src/cuda/module.cu
@@ -13,7 +13,7 @@ __device__ float calculate_dist(float avg, float item) {
 	float diff = (avg - item);
 	return diff * diff;
 }
-float calculate(float avg, float item) {
+float calculate_distance(float avg, float item) {
 	float diff = (avg - item);
 	return diff * diff;
 }
@@ -168,124 +168,56 @@ void call_cluster_sum_size_kernel(
 	cudaFree(d_cluster_size);
 }
 
-__global__ void block_dist_row_labels(
-	const float* matrix, 
-	int i,
-	int k,
-	const int* col_labels,
-	const float* cluster_avg,
-	double* dist_array,
-	int num_cols,
-	int num_col_labels) 
-{
-	__shared__ double sdata[1024];
+__global__ void row_labels_iteration() {
 
-	int j = blockDim.x * blockIdx.x + threadIdx.x; 
-	int tid = threadIdx.x;
-
-	if (j < num_cols) {
-		float item = matrix[i * num_cols + j];
-
-		int row_label = k;
-		int col_label = col_labels[j];
-
-		float y = cluster_avg[row_label * num_col_labels + col_label];
-
-		sdata[tid] = (y - item) * (y - item);
-	}
-
-	__syncthreads();
-
-	// do reduction in shared mem
-	for (int s=1; s < blockDim.x; s *= 2) {
-		if (tid % (2*s) == 0) {
-			sdata[tid] += sdata[tid + s];
-		}
-		__syncthreads();
-	}
-
-	// write result for this block to global mem
-	if (tid == 0) {
-		dist_array[blockIdx.x] = sdata[0];
-	}
 }
 
-std::pair<int, double> best_row_label(
-	int num_row_labels,
-	int num_col_labels,
-	int num_rows,
-	int num_cols,
-	const float* matrix,
-	const float* cluster_avg,
-	int i,
-	const int* col_labels) {
-	int N = num_cols;
+std::pair<int, double> call_update_row_labels_kernel(
+    int num_rows,
+    int num_cols,
+    int num_row_labels,
+    int num_col_labels,
+    const float* matrix,
+    int* row_labels,
+    const int* col_labels,
+    const float* cluster_avg,
+    int displacement) {
+    int num_updated = 0;
+    double total_dist = 0;
 
-	// Block size and number calculation
-	int blockSize = 1024;
-  int numBlocks = (N + blockSize - 1) / blockSize;
-	
-	// Number of bytes to allocate for numBlocks
-	size_t bytes = numBlocks*sizeof(double);
+    for (int i = 0; i < num_rows; i++) {
+        int best_label = -1;
+        double best_dist = INFINITY;
+        int displaced_i = i + displacement;
 
-	// Allocate memory on host
-	double *dist_blocks = (double*)malloc(bytes);
+        for (int k = 0; k < num_row_labels; k++) {
+            double dist = 0;
 
-	// Allocate memory on device
-	double *d_dist_blocks;
-	cudaMalloc(&d_dist_blocks, bytes);
-	float *d_matrix;
-	cudaMalloc(&d_matrix, (num_cols*num_rows)*sizeof(float));
-	float *d_cluster_avg;
-	cudaMalloc(&d_cluster_avg, (num_row_labels*num_col_labels)*sizeof(float));
-	int *d_col_labels;
-	cudaMalloc(&d_col_labels, num_cols*sizeof(int));
+            for (int j = 0; j < num_cols; j++) {
+                float item = matrix[displaced_i * num_cols + j];
 
-	// Copy data to device
-	cudaMemcpy(d_matrix, matrix, (num_cols*num_rows)*sizeof(float), cudaMemcpyHostToDevice);
-	cudaMemcpy(d_cluster_avg, cluster_avg, (num_row_labels*num_col_labels)*sizeof(float), cudaMemcpyHostToDevice);
-	cudaMemcpy(d_col_labels, col_labels, num_cols*sizeof(int), cudaMemcpyHostToDevice);
+                int row_label = k;
+                int col_label = col_labels[j];
+                float y = cluster_avg[row_label * num_col_labels + col_label];
 
-	int best_label = -1;
-	double best_dist = INFINITY;
+                dist += calculate_distance(y, item);
+            }
 
-	for (int k = 0; k < num_row_labels; k++) {
-		double dist = 0;
+            if (dist < best_dist) {
+                best_dist = dist;
+                best_label = k;
+            }
+        }
 
-		block_dist_row_labels<<< numBlocks, blockSize >>>(
-			d_matrix,
-			i,
-			k,
-			d_col_labels,
-			d_cluster_avg,
-			d_dist_blocks, 
-			num_cols,
-			num_col_labels);
+        if (row_labels[i] != best_label) {
+            row_labels[i] = best_label;
+            num_updated++;
+        }
 
-		// Copy result from device to host
-		cudaMemcpy(dist_blocks, d_dist_blocks, bytes, cudaMemcpyDeviceToHost);
+        total_dist += best_dist;
+    }
 
-		// Reduce result by summing all block results
-		double sum = 0;
-		for (int x = 0; x < numBlocks; x++) {
-			sum += dist_blocks[x];
-		}
-
-		dist = sum;
-
-		if (dist < best_dist) {
-			best_dist = dist;
-			best_label = k;
-		}
-	}
-
-	// Free allocated memory
-	cudaFree(d_dist_blocks);
-	cudaFree(d_matrix);
-	cudaFree(d_col_labels);
-	free(dist_blocks);
-
-	return {best_label, best_dist};
+    return {num_updated, total_dist};
 }
 
 
