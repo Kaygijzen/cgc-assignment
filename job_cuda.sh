892c3bff2e5b01590ad65416a1d9e99783516c69
HPC course Leiden <kaygijzen98@gmail.com>
2024-01-19 23:22:27 +0100

    scripts

--- job_cuda.sh
+++ job_cuda.sh
@@ -3,4 +3,4 @@
 #SBATCH -N 16
 #SBATCH --gres=gpu:1
 
-mpirun -np 16 bin/cgc_cuda /var/scratch/bwn200/HPC_data/spring_data_m.npy /var/scratch/bwn200/HPC_data/spring_labels_m_5x100.txt --max-iterations 125 --output "cuda.txt"
+mpirun -np 16 cgc_cuda /var/scratch/bwn200/HPC_data/spring_data_m.npy /var/scratch/bwn200/HPC_data/spring_labels_m_5x100.txt --max-iterations 125 --output "cuda.txt"
