b065478896f1413ca046ab36c9548410b5c5d4d9
HPC course Leiden <kaygijzen98@gmail.com>
2024-01-19 22:22:57 +0100

    remove script

--- job_openmp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH --time=00:15:00
-#SBATCH -N 4
-#SBATCH --ntasks-per-node=1
-
-mpirun -np 4 cgc_openmp /var/scratch/bwn200/HPC_data/spring_data_l.npy /var/scratch/bwn200/HPC_data/spring_labels_l_5x100.txt --max-iterations 10  --output "openmp.txt"
